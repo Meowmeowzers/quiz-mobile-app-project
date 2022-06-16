bb095154e877b754ad0775cf3d77a87af8d3d109
Meowmeowzers <95127319+Meowmeowzers@users.noreply.github.com>
2022-06-16 10:10:50 +0800

    - Added framents QuizProblemChoicesEditFragment.kt and QuizProblemsListFragment.kt - Added respective xml layouts for QuizProblemsListFragment.kt and QuizProblemChoicesEditFragment.kt - New items in res_navigation.xml - Refactored some id views in xml files and its references - New strings in strings.xml
    -  4 added
    -  5 modified
    -  deleted
    -  3 warning/s
    -  error/s

--- app/src/main/java/com/bscs3a_2022/quizappproject2/quiz/QuizProblemsListFragment.kt
+++ app/src/main/java/com/bscs3a_2022/quizappproject2/quiz/QuizProblemsListFragment.kt
@@ -1,19 +1,18 @@
 package com.bscs3a_2022.quizappproject2.quiz
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.bscs3a_2022.quizappproject2.databinding.QuizProblemsBinding
 import com.bscs3a_2022.quizappproject2.quiz.database.QuizDatabase
-import com.bscs3a_2022.quizappproject2.databinding.QuizListBinding
-import com.bscs3a_2022.quizappproject2.quiz.viewmodel_factory.QuizListViewModel
-import com.bscs3a_2022.quizappproject2.quiz.viewmodel_factory.QuizListViewModelFactory
 
-class QuizListFragment : Fragment() {
-    private var _binding: QuizListBinding? = null
+
+class QuizProblemsListFragment: Fragment() {
+    private var _binding: QuizProblemsBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,26 +25,25 @@ class QuizListFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = QuizListBinding.inflate(inflater, container, false)
+        _binding = QuizProblemsBinding.inflate(inflater, container, false)
         (activity as AppCompatActivity).supportActionBar?.title = "Quiz"
 
         val application = requireNotNull(this.activity).application
         val dataSource = QuizDatabase.getInstance(application).quizSetDatabaseDao
-        val viewModelFactory = QuizListViewModelFactory(dataSource, application)
-        val viewModel =
-            ViewModelProvider(this, viewModelFactory)[QuizListViewModel::class.java]
-        binding.lifecycleOwner = viewLifecycleOwner
+//        val viewModelFactory = QuizListViewModelFactory(dataSource, application)
+//        val viewModel =
+//            ViewModelProvider(this, viewModelFactory)[QuizListViewModel::class.java]
+//        binding.lifecycleOwner = viewLifecycleOwner
 
-        binding.quizViewModel = viewModel
-        val adapter = QuizListAdapter()
-        binding.quizListRecycler.adapter = adapter
+//        binding.quizViewModel = viewModel
+//        val adapter = QuizListAdapter()
+//        binding.quizListRecycler.adapter = adapter
 
-        viewModel.quizList.observe(viewLifecycleOwner) {
-            it?.let {
-                adapter.submitList(it)
-            }
-        }
-
+//        viewModel.quizList.observe(viewLifecycleOwner) {
+//            it?.let {
+//                adapter.submitList(it)
+//            }
+//        }
         return binding.root
     }
 
